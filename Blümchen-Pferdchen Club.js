0ad602fd837cccbfc042f33eee44e6360a487e53
fluecubic <jonas.lorenz1234567890@gmail.com>
2024-06-17 09:01:14 +0200

    Update Blümchen-Pferdchen Club.js

--- Blümchen-Pferdchen Club.js
+++ Blümchen-Pferdchen Club.js
@@ -91,11 +91,7 @@ function stop(audioElement) {
  
  //logbuch
  let seite = 1;
- let seite1 = "Logbuch<br>
-    Ränge:<br>5 Sterne:<br>Hannes,
-    Paul,Milo,<br>Richter:<br>Aaron<br>4 Sterne:<br>
-    Jonas<br>3 Sterne:<br>Jannik,Anton,Max im zoo<br>
-    2 Sterne:<br>Jonathan,Sjard,Helene,Mathilda";
+ let seite1 = "Logbuch<br>Ränge:<br>5 Sterne:<br>Hannes,Paul,Milo,<br>Richter:<br>Aaron<br>4 Sterne:<br>Jonas<br>3 Sterne:<br>Jannik,Anton,Max im zoo<br> 2 Sterne:<br>Jonathan,Sjard,Helene,Mathilda";
  let seite2 = "seite 2";
  let seite3 = "seite 3";
  var arrowleft = document.getElementById("arrowleft");
